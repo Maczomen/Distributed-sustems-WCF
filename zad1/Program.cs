d6ded6c2564a6f5eea8e0b1d60ce8ffe7e747199
Maczomen <maczomen2000@gmail.com>
2022-06-27 09:16:28 +0200

    full implementation + list and discription of tasks + test.exe

---
+++ zad1/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ServiceModel;
+using KSR_WCF1;
+
+
+namespace zad1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var fact = new ChannelFactory<IZadanie1>(
+                new NetNamedPipeBinding(),
+                new EndpointAddress("net.pipe://localhost/ksr-wcf1-test"));
+
+            var client = fact.CreateChannel();
+            Console.WriteLine(client.Test("dzialajacy test zad1"));
+
+            var client2 = new ServiceReference1.Zadanie2Client();
+            Console.WriteLine("dzialajacy test zad3");
+
+            try
+            {
+                client.RzucWyjatek(true);
+            }
+            catch (FaultException<Wyjatek> e)
+            {
+                Console.WriteLine(client.OtoMagia(e.Detail.magia));
+            }
+
+            try
+            {
+                client.RzucWyjatek(true);
+            }
+            catch (FaultException<Wyjatek> e)
+            {
+                Console.WriteLine(client.OtoMagia(e.Detail.magia));
+            }
+
+            var client3 = new ServiceReference2.Zadanie7Client();
+
+            try
+            {
+                client3.RzucWyjatek7("wyjatek", 7);
+            }
+            catch (FaultException<ServiceReference2.Wyjatek7> e)
+            {
+                Console.WriteLine(e);
+            }
+
+            Console.ReadKey();
+            ((IDisposable)client2).Dispose();
+            ((IDisposable)client).Dispose();
+            fact.Close();
+        }
+    }
+}
---
+++ zad2/Program.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ServiceModel;
+using KSR_WCF1;
+using System.ServiceModel.Description;
+using System.Runtime.Serialization;
+
+[ServiceContract]
+public interface IZadanie2
+{
+    [OperationContract]
+    string Test(string arg);
+}
+
+public class Zadanie2: IZadanie2
+{
+    public string Test(string arg)
+    {
+        return $"Testowany: {arg}";
+    }
+}
+
+
+[DataContract]
+public class Wyjatek7
+{
+    [DataMember]
+    public string opis { get; set; }
+    [DataMember]
+    public string a { get; set; }
+    [DataMember]
+    public int b { get; set; }
+}
+
+[ServiceContract]
+public interface IZadanie7
+{
+    [OperationContract]
+    [FaultContract(typeof(Wyjatek7))]
+    void RzucWyjatek7(string a, int b);
+}
+
+public class Zadanie7 : IZadanie7
+{
+    public void RzucWyjatek7(string a, int b)
+    {
+        var exept = new FaultException<Wyjatek7>(new Wyjatek7(),
+            new FaultReason($"Zad 67 dziala {a} {b}"));
+        throw exept;
+    }
+}
+
+
+namespace zad2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var host = new ServiceHost(typeof(Zadanie2));
+            host.AddServiceEndpoint(typeof(IZadanie2),
+                new NetNamedPipeBinding(),
+                "net.pipe://localhost/ksr-wcf1-zad2");
+
+            var b = host.Description.Behaviors.Find<ServiceMetadataBehavior>();
+            if (b == null) b = new ServiceMetadataBehavior();
+            host.Description.Behaviors.Add(b);
+
+            host.AddServiceEndpoint(ServiceMetadataBehavior.MexContractName,
+                MetadataExchangeBindings.CreateMexNamedPipeBinding(),
+                "net.pipe://localhost/metadane");
+
+            host.AddServiceEndpoint(typeof(IZadanie2),
+                new NetTcpBinding(),
+                "net.tcp://127.0.0.1:55765");
+
+            host.Open();
+
+
+            var host2 = new ServiceHost(typeof(Zadanie7));
+            host2.AddServiceEndpoint(typeof(IZadanie7),
+                new NetNamedPipeBinding(),
+                "net.pipe://localhost/ksr-wcf1-zad7");
+
+            var c = host2.Description.Behaviors.Find<ServiceMetadataBehavior>();
+            if (c == null) c = new ServiceMetadataBehavior();
+            host2.Description.Behaviors.Add(c);
+
+            host2.AddServiceEndpoint(ServiceMetadataBehavior.MexContractName,
+                MetadataExchangeBindings.CreateMexNamedPipeBinding(),
+                "net.pipe://localhost/metadane2");
+
+            host2.Open();
+
+            Console.ReadKey();
+            host2.Close();
+            host.Close();
+        }
+    }
+}
