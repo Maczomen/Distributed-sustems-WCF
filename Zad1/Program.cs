65184185cb5afb83195ceef87acf76d03dec908a
Maczomen <maczomen2000@gmail.com>
2022-06-27 09:19:42 +0200

    full implementation + list of tasks and short discription + test.exe

---
+++ Zad1/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zad1
+{
+    class Program
+    {
+        public class Handler: ServiceReference2.IZadanie2Callback
+        {
+            public void Zadanie(string zadanie, int pkt, bool zaliczone)
+            {
+                Console.WriteLine($"{zadanie} na {pkt}% zaliczone: {zaliczone}");
+            }
+        }
+        static void Main(string[] args)
+        {
+            var client1 = new ServiceReference1.Zadanie1Client();
+            IAsyncResult result = client1.BeginDlugieObliczenia(null, null);
+
+            for (int i = 0; i <= 20; i++)
+            {
+                client1.Szybciej(i, 3 * i * i - 2 * i);
+            }
+
+            result.AsyncWaitHandle.WaitOne();
+            Console.WriteLine(client1.EndDlugieObliczenia(result));
+            Console.ReadKey();
+            ((IDisposable)client1).Dispose();
+
+            var client2 = new ServiceReference2.Zadanie2Client(new System.ServiceModel.InstanceContext(new Handler()));
+            client2.PodajZadania();
+            Console.ReadKey();
+            ((IDisposable)client2).Dispose();
+        }
+    }
+}
---
+++ Zad3_4/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using KSR_WCF2;
+using System.ServiceModel;
+
+
+namespace Zad3_4
+{
+    public class Zadanie3: IZadanie3
+    {
+        public void TestujZwrotny()
+        {
+            var zwr = OperationContext.Current.GetCallbackChannel<IZadanie3Zwrotny>();
+            for (int i=0;i <= 30; i++)
+            {
+                zwr.WolanieZwrotne(i, i * i * i - i * i);
+            }
+
+        }
+    }
+
+    [ServiceBehavior(InstanceContextMode = InstanceContextMode.PerSession)]
+    public class Zadanie4 : IZadanie4
+    {
+        public int counter;
+        public int Dodaj(int v)
+        {
+            counter = counter + v;
+            return counter;
+        }
+
+        public void Ustaw(int v)
+        {
+            counter = v;
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var host = new ServiceHost(typeof(Zadanie3));
+            host.AddServiceEndpoint(typeof(IZadanie3),
+                new NetNamedPipeBinding(),
+                "net.pipe://localhost/ksr-wcf2-zad3");
+            var host2 = new ServiceHost(typeof(Zadanie4));
+            host2.AddServiceEndpoint(typeof(IZadanie4),
+                new NetNamedPipeBinding(),
+                "net.pipe://localhost/ksr-wcf2-zad4");
+
+            host.Open();
+            host2.Open();
+            Console.ReadKey();
+            host2.Close();
+            host.Close();
+
+        }
+    }
+}
